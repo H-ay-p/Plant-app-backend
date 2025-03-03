3cd77fa268752d8e875e9fb9f1c000931e739a95
RachelKellyTech <133110334+RachelKellyTech@users.noreply.github.com>
2025-03-03 17:02:24 +0000

    modify getWeather function

--- db/data/getWeather.js
+++ db/data/getWeather.js
@@ -1,8 +1,14 @@
+const key = "25e2198b305c4bc5bf7122151252602"
+
+
 function getWeather() {
-    fetch("http://api.weatherapi.com/v1/forecast")
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(error => console.error("Error fetching weather data:", error));
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=${key}&q=London&days=1&aqi=no&alerts=no`)
+      .then((response)=> {
+        return response.json()
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error)
+      })
   }
   
   module.exports = getWeather
