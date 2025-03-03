ac366f01b7752cfdf8216dc8b69a05b0dd2fff2e
RachelKellyTech <133110334+RachelKellyTech@users.noreply.github.com>
2025-03-03 11:22:02 +0000

    add api endpoints test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,5 @@
 const app = require('../app.js')
+const endpoints = require("../endpoints.json");
 const request = require("supertest");
 const db = require("../db/connection.js");
 const seed = require("../db/seed.js")
@@ -12,3 +13,14 @@ beforeEach(() => {
     return db.end(); 
   });
   
+
+  describe("GET /api", () => {
+    test("200: Responds with an object detailing the documentation for each endpoint", () => {
+      return request(app)
+        .get("/api")
+        .expect(200)
+        .then(({ body: { endpoints } }) => {
+          expect(endpoints).toEqual(endpoints);
+        });
+    });
+  });
