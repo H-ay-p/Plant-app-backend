f1c978020a49660f0e7f6b8ea1d7e4420c191f4c
RachelKellyTech <133110334+RachelKellyTech@users.noreply.github.com>
2025-03-03 10:49:42 +0000

    add utils.test.js file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,6 @@
 const app = require('../app.js')
-// const request = require("supertest");
-// const db = require("../db/connection.js");
+const request = require("supertest");
+const db = require("../db/connection.js");
 const seed = require("../db/seed.js")
 
 
