0c170f0d0f7244386e49e52a8b60b0995400e0c3
RachelKellyTech <133110334+RachelKellyTech@users.noreply.github.com>
2025-03-03 15:25:45 +0000

    add getWeather function to api.js

--- Data_collection_files/api.js
+++ Data_collection_files/api.js
@@ -43,9 +43,12 @@ getPlantById(id))
 
 getPlants()
 
-
-async function getWeatehr() {
-  const response = await fetch(
-    ``
-  )
+function getWeather() {
+  fetch("http://api.weatherapi.com/v1/forecast")
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.error("Error fetching weather data:", error));
 }
+
+
+module.exports = getWeather;
