eff41e34940b80795a0f6a7fe831d38768b54dd2
“Hayp” <hanna.y.peters@gmail.com>
2025-06-02 12:41:03 +0100

    testing

--- listen.js
+++ listen.js
@@ -1,5 +1,10 @@
 const app = require("./app.js");
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+console.log("Current environment variables:");
+console.log({
+  DATABASE_URL: process.env.DATABASE_URL,
+  API_BASE_URL: process.env.API_BASE_URL,
+});
 
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
